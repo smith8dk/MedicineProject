286d3ef14b7471bc712eaf1f990381a02eb0a7be
smith8dk <109267136+smith8dk@users.noreply.github.com>
2023-07-21 20:22:46 -0400

    Add project files.

---
+++ MedicineProject/MedicineProject/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using MedicineProject.ViewModels;
+
+namespace MedicineProject.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ MedicineProject/MedicineProject/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using MedicineProject.Models;
+using MedicineProject.ViewModels;
+
+namespace MedicineProject.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
